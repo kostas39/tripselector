33bc330bf64e970375d0925c20cfe758192ddc87
arnold cubici-jones <arnoldcubicijones@hotmail.co.uk>
2022-12-06 10:53:45 +0000

    Reloads instance of DB without errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_141938) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "journeys", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.date "start_date"
-    t.date "end_date"
-    t.string "tag"
-    t.text "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.index ["user_id"], name: "index_journeys_on_user_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_151548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_141938) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "cities"
   add_foreign_key "city_journeys", "cities"
   add_foreign_key "city_journeys", "journeys"
   add_foreign_key "journeys", "users"
