c1779c145f0eef3112b9b9a2b10272d3c7181b8c
Victor de Quercize <vdequercize@hotmail.com>
2022-12-06 11:08:05 +0100

    getting ready

--- db/schema.rb
+++ db/schema.rb
@@ -14,23 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_141938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "journeys", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.date "start_date"
-    t.date "end_date"
-    t.string "tag"
-    t.text "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.index ["user_id"], name: "index_journeys_on_user_id"
-  end
-
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_135938) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.string "tags"
@@ -81,5 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_135938) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "cities"
   add_foreign_key "journeys", "users"
 end
