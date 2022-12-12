e5763e778ab30f1a914113e07a9d3bea4c913ddb
Victor de Quercize <vdequercize@hotmail.com>
2022-12-12 18:41:37 +0100

    added all metadatas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :bio, :photo, :favourite, :second, :third])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
