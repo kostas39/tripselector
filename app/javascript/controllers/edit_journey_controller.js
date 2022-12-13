769a15cb3204b3752a063d01e69c028d1f707c34
Victor de Quercize <vdequercize@hotmail.com>
2022-12-13 14:28:44 +0100

    the edit feature now works on the card you're trying to update

--- app/javascript/controllers/edit_journey_controller.js
+++ app/javascript/controllers/edit_journey_controller.js
@@ -3,26 +3,33 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="edit-journey"
 export default class extends Controller {
   static targets = ["infos", "form", "card", "title", "show"]
+
   connect() {
+    console.log("hello once again")
   }
 
-  displayForm() {
-    this.infosTarget.classList.add("d-none")
-    this.formTarget.classList.remove("d-none")
+  displayForm(event) {
+    const element = event.target
+    const index = element.dataset.index
+    this.infosTargets[index].classList.add("d-none")
+    this.formTargets[index].classList.remove("d-none")
   }
 
   update(event) {
     event.preventDefault()
-    const url = this.formTarget.action
+    const element = event.target
+    console.log(element)
+    const index = element.dataset.index
+    const url = this.formTargets[index].action
     fetch(url, {
       method: "PATCH",
       headers: { "Accept": "text/plain" },
-      body: new FormData(this.formTarget)
+      body: new FormData(this.formTargets[index])
     })
       .then(response => response.text())
-
       .then((data) => {
         this.showTarget.innerHTML = data
+        console.log(data)
       })
   }
 
